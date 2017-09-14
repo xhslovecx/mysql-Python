965940409465ece2ad9ef011673659d14b293b64
xhslovecx <31601833+xhslovecx@users.noreply.github.com>
2017-09-14 22:09:45 +0800

    第一次修改--插入变量
    在表中插入Python的数组元素

--- Python调用mysql_1.py
+++ Python调用mysql_1.py
@@ -20,23 +20,28 @@ conn = pymysql.connect(
 cur=conn.cursor() #获取对应的操作游标  
 #information=a[id,name,sex,age,high,salary,vocation]
 temp=[]
-#temp=[3456,'xhs','女',26,160,10000,'计算机']
+temp=[100020,'xhs','女',26,160,10000,'计算机']
+
 #在数据库中插入一条数据
 sql="insert into woman(id,name,sex,age,high,salary,vocation)values(10006,'xcx','男',26,180,10000,'计算机')"
 cur.execute(sql)
+
 ##在数据库中插入多条数据
 sqls="insert into woman values(%s,%s,%s,%s,%s,%s,%s)"
 cur.executemany(sqls,[
+        (temp[0],temp[1],temp[2],temp[3],temp[4],temp[5],temp[6])
         (100010,'xcx','男',26,180,10000,'计算机')
         ,(100011,'马明明','女',26,159,10000,'计算机')
         ,(100012,'陈欣','男',26,178,10000,'计算机')
         ])
+
 #输出查询的元组个数
 sql2='select * from woman'
 print(cur.execute(sql2))
 #删除和插入一条记录
 cur.execute("delete from woman where id=100010")
 cur.execute("update woman set name='曹伟华' where id=10007")
+
 #关闭游标
 cur.close()
 #conn.commit()方法在提交事物，在向数据库插入一条数据时必须要有这个方法，否则数据不会被真正的插入。
